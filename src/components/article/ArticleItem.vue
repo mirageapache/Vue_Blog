3c7721ab5619e00968b8667070c842d5624fb2af
jameswang14 <james.wang@settour.com.tw>
2025-05-07 11:35:14 +0800

    [feat]安裝Tiptap富文本編輯器套件

--- src/components/article/ArticleItem.vue
+++ src/components/article/ArticleItem.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import moment from 'moment';
 import { formatDateTime } from '@/utils/dateTime';
 import { HINT_LABEL } from '@/constants/LayoutConstants';
 import type { ArticleDataType } from '@/types/articleType';
 import UserInfoPanel from '../user/UserInfoPanel.vue';
+import { useEditor } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
 
 const props = defineProps<{
   articleData: ArticleDataType;
@@ -17,16 +19,29 @@ const article = ref(articleData);
 const { _id, title, createdAt, author } = article.value;
 const showCreateTip = ref(false);
 
+const editor = useEditor({
+  content: articleData.content || '',
+  editable: false,
+  extensions: [StarterKit]
+});
 
+const htmlContent = ref('');
 
+onMounted(() => {
+  if (editor.value) {
+    htmlContent.value = editor.value.getHTML();
+  }
+});
 </script>
 
 <template>
   <div
     class="text-left border-b-[1px] p-1 sm:p-2 dark:border-gray-700 last:border-b-0 hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer"
-    @click="() => {
-      router.push(`/article/${_id}`);
-    }"
+    @click="
+      () => {
+        router.push(`/article/${_id}`);
+      }
+    "
   >
     <div class="flex w-full justify-between">
       <UserInfoPanel
@@ -40,8 +55,8 @@ const showCreateTip = ref(false);
       <div class="flex flex-col justify-center items-end">
         <span
           class="relative my-0.5"
-          @mouseenter="() => showCreateTip = true"
-          @mouseleave="() => showCreateTip = false"
+          @mouseenter="() => (showCreateTip = true)"
+          @mouseleave="() => (showCreateTip = false)"
         >
           <p class="text-gray-600 dark:text-gray-300">{{ formatDateTime(createdAt) }}</p>
           <span
@@ -55,7 +70,7 @@ const showCreateTip = ref(false);
     </div>
     <div class="pb-3">
       <h2 class="font-semibold text-2xl xl:text-3xl">
-        <span class="hover:underline hover:text-orange-500">{title}</span>
+        <span class="hover:underline hover:text-orange-500">{{ title }}</span>
       </h2>
       <div
         class="max-h-[150px] text-gray-600 dark:text-gray-300 line-clamp-5"
@@ -64,3 +79,13 @@ const showCreateTip = ref(false);
     </div>
   </div>
 </template>
+
+<style>
+.ProseMirror {
+  outline: none;
+}
+
+.ProseMirror p {
+  margin: 0;
+}
+</style>
